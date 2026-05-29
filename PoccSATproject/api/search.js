05702e43cbc61f05f304272ad15ca2738e7a1b90
elton pw <elton.weng@student.citrakasih.sch.id>
2026-05-29 19:40:00 +0800

    cek

--- PoccSATproject/api/search.js
+++ PoccSATproject/api/search.js
@@ -5,17 +5,11 @@ export default async function handler(req, res) {
       api_key: process.env.SERPAPI_KEY,
     });
 
-    const response = await fetch(
-      `https://serpapi.com/search?${query}`
-    );
-
+    const response = await fetch(`https://serpapi.com/search?${query}`);
     const data = await response.json();
-
-    return res.status(200).json(data);
-
+    res.json(data);
   } catch (err) {
-    return res.status(500).json({
-      error: err.message,
-    });
+    res.status(500).json({ error: err.message });
   }
 }
+
